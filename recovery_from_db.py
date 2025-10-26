7bdfde13d2f2d311aa03cc25ebce1724cc271067
xd <2080776216@qq.com>
2025-10-26 20:03:23 +0800

    improve

--- recovery_from_db.py
+++ recovery_from_db.py
@@ -42,6 +42,7 @@ def recoveryFromLocalDB(db: ComicDB):
             continue
         file_path = comic_info[2]
         local_path = os.path.join(BASE_PATH, file_path)
+        file_hash = file_path.split('.')[0]
         if os.path.exists(local_path):
             continue
         print(f"ID:{comic_id} 本地文件不存在: {file_path}")
@@ -53,17 +54,17 @@ def recoveryFromLocalDB(db: ComicDB):
             print(f'ID:{comic_id} 无源ID')
             continue
         print(f"检索到ID:{comic_id} 的源ID: {source_comic_id}.")
-        try:
-            comic = hitomi_instance.get_comic(source_comic_id)
-            download_path = comic.download(max_threads=5)
-            if download_path:
-                # Rename and move the downloaded file
-                shutil.move(download_path, local_path)
-                print(f"ID:{comic_id} 文件还原成功: {file_path}")
-            else:
-                print(f"ID:{comic_id} 文件还原失败, 源ID:{source_comic_id}")
-        except Exception as e:
-            print(f"An error occurred while downloading comic with source ID {source_comic_id}: {e}")
+        comic = hitomi_instance.get_comic(source_comic_id)
+        download_path = comic.download(max_threads=5)
+        download_file_hash = getFileHash(download_path)
+        if download_file_hash != file_hash:
+            raise AssertionError('哈希校验失败')
+        if download_path:
+            # Rename and move the downloaded file
+            shutil.move(download_path, local_path)
+            print(f"ID:{comic_id} 文件还原成功: {file_path}")
+        else:
+            print(f"ID:{comic_id} 文件下载失败, 源ID:{source_comic_id}")
 
 
 def updateFromRemoteDB(remote_db: ComicDB, local_db: ComicDB):
